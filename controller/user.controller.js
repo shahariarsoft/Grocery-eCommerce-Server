3a373ab7026e214fb39d94a74fe6991f790c2101
Shahariar <mononshahariar@gmail.com>
2026-02-10 13:21:03 +0600

    user.controller.js

---
+++ controller/user.controller.js
@@ -0,0 +1,48 @@
+import User from "../models/user.model.js";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+
+// register user : /api/user/register
+
+export const registerUser = async (req, res) => {
+    try {
+        const { name, email, password } = req.body;
+        if (!name || !email, !password){
+            return res
+            .status(400)
+            .json({ message: "All fields are required", success: false });
+        }
+        const existingUser = await User.findOne({ email });
+        if (existingUser) {
+            return res 
+            .status(400)
+            .json({ message: "User already exists", success: false });
+        }
+        const hashedPassword = await  bcrypt.hash(password, 10);
+        const user = await User.create({
+            name,
+            email,
+            password: hashedPassword,
+        });
+        const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+            expiresIn: "7d",
+        });
+        res.cookie("token", token, {
+            httpOnly: true,
+            secure: process.env.NODE_ENV == "production",
+            sameSite: process.env.NODE_ENV == "production" ? "none" : "Strict",
+            maxAge: 7 * 24 * 60 * 60 * 1000,
+        });
+        res.json({
+            message: "User registered successfully",
+            success: true,
+            user: {
+                name: user.name,
+                email: user.email,
+            },
+        });
+    } catch (error) {
+        console.log (error);
+        res.status(500).json({ message: "Internal server error"});
+    }
+};
--- index.js
+++ index.js
@@ -4,6 +4,7 @@ import cookieParser from "cookie-parser";
 import dotenv from "dotenv";
 import { connectDB } from "./config/connectDB.js";
 dotenv.config();
+import userRoutes from "./routes/user.routes.js"
 const app = express();
 
 connectDB()
@@ -18,6 +19,8 @@ app.use(cookieParser());
 app.get("/", (req, res) => {
     res.send("Hello World");
 });
+
+app.use("/api/user", userRoutes);
 
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
