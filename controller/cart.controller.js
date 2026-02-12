30fdc5f9f01d6b7201e0cadd8b0d4b0b4c549b31
Shahariar <mononshahariar@gmail.com>
2026-02-12 23:50:37 +0600

    cart.controller.js

---
+++ controller/cart.controller.js
@@ -0,0 +1,29 @@
+import User from "../models/user.model.js";
+
+// update user cartData: /api/cart/update
+
+export const updateCart = async (req, res) => {
+    try {
+        const userId =req.user;
+        const { cartItems } = req.body;
+        const updatedUser = await User.findByIdAndUpdate(
+            userId,
+            { cartData: cartItems },
+            { new: true }
+        );
+        if (!updatedUser) {
+            return res
+            .status(404)
+            .json({ message: "User not found", success: false });
+        }
+        res
+        .status(200)
+        .json({ 
+            updatedUser,
+            success: true,
+            message: "Cart update successfully",
+        });
+    } catch (error) {
+        res.status(500).json({ message: "Server error", error: error.message })
+    }
+};
