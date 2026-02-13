311c975e14d9dc68bd3a4a635b53555be13d0361
Shahariar <mononshahariar@gmail.com>
2026-02-13 15:57:20 +0600

    get address

--- controller/address.controller.js
+++ controller/address.controller.js
@@ -14,9 +14,24 @@ export const addAddress = async (req, res) => {
         res.status(201).json({
             message: "Address added successfully",
             success: true,
-        })
+        });
     } catch (error) {
         console.error("Error adding address:", error);
+        res.status(500).json({ message: "Internal server error" });
+    }
+};
+
+// get address: /api/address/get
+export const getAddress =async (req, res) => {
+    try {
+        const userId = req.user;
+        const addresses = (await Address.find({ userId })).toSorted({ createdAt: -1 });
+            res.status(200).json({
+                success: true,
+            addresses,
+        });
+    } catch (error) {
+        console.error("Error fetching address:", error);
         res.status(500).json({ message: "Internal server error" });
     }
 };
